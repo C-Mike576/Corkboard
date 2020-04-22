dc1d51e5f3cadc4ceaad413c6a454993cdf9d29a
C-Mike576 <m.calkins576@gmail.com>
2020-04-22 16:19:39 -0400

    working on async redux

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,3 +1,15 @@
 function fetchPosts() {
+    fetch("http://localhost:4000/posts")
+    .then(res => res.json)
+    .then(postsObj => renderPost(postsObj))
     
+}
+
+function renderPost(postsObj) {
+    postsObj.map(post => {
+        dispatch({
+            type: "ADD_POST",
+            payload: post
+        })
+    })
 }
